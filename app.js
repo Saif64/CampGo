4e7a974941ef8a7f290c721b409e35b0bbc1bc43
Saif64 <saifmahmud81@gmail.com>
2022-09-25 10:45:40 +0600

    dummy data

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const app = express();
 const PORT = 3000;
+const path = require("path");
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 app.get("/", (req, res) => {
-  res.send("hello ");
+  res.render("home");
 });
 
 app.listen(PORT, () => {
